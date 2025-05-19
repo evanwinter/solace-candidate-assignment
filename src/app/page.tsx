6aff425e84beb701d4d26dd62c3fa8ac9b7b0946
Evan Winter <evan@evanwinter.me>
2025-05-19 11:52:05 -0500

    feat: implement API-route based filtering, query from db, use Ant Design for FE components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,30 @@
-"use client";
+import { Space } from "antd";
+import { AdvocatesSearch } from "./components/search";
+import { AdvocatesTable } from "./components/table";
 
-import { useEffect, useState } from "react";
-
-export default function Home() {
-  const [advocates, setAdvocates] = useState([]);
-  const [filteredAdvocates, setFilteredAdvocates] = useState([]);
-
-  useEffect(() => {
-    console.log("fetching advocates...");
-    fetch("/api/advocates").then((response) => {
-      response.json().then((jsonResponse) => {
-        setAdvocates(jsonResponse.data);
-        setFilteredAdvocates(jsonResponse.data);
-      });
-    });
-  }, []);
-
-  const onChange = (e) => {
-    const searchTerm = e.target.value;
-
-    document.getElementById("search-term").innerHTML = searchTerm;
-
-    console.log("filtering advocates...");
-    const filteredAdvocates = advocates.filter((advocate) => {
-      return (
-        advocate.firstName.includes(searchTerm) ||
-        advocate.lastName.includes(searchTerm) ||
-        advocate.city.includes(searchTerm) ||
-        advocate.degree.includes(searchTerm) ||
-        advocate.specialties.includes(searchTerm) ||
-        advocate.yearsOfExperience.includes(searchTerm)
-      );
-    });
-
-    setFilteredAdvocates(filteredAdvocates);
-  };
-
-  const onClick = () => {
-    console.log(advocates);
-    setFilteredAdvocates(advocates);
-  };
-
+export default async function Home() {
   return (
-    <main style={{ margin: "24px" }}>
-      <h1>Solace Advocates</h1>
-      <br />
-      <br />
-      <div>
-        <p>Search</p>
-        <p>
-          Searching for: <span id="search-term"></span>
-        </p>
-        <input style={{ border: "1px solid black" }} onChange={onChange} />
-        <button onClick={onClick}>Reset Search</button>
+    <main className="max-w-full box-border mx-auto p-4 lg:p-8">
+      <Space direction="vertical" size="large">
+        <header className="grid gap-4 max-w-4xl">
+          <h1 className="text-4xl tracking-tight text-pretty">
+            Solace Advocates
+          </h1>
+          <p className="text-base text-gray-700 text-balance">
+            Solace Advocates is a platform that connects individuals with
+            advocates who can provide support and guidance. Our mission is to
+            empower individuals by providing access to a network of qualified
+            advocates who can help them navigate the complexities of their
+            situations.
+          </p>
+        </header>
+        <div className="max-w-md">
+          <AdvocatesSearch />
+        </div>
+      </Space>
+      <div className="w-full mt-8">
+        <AdvocatesTable />
       </div>
-      <br />
-      <br />
-      <table>
-        <thead>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>City</th>
-          <th>Degree</th>
-          <th>Specialties</th>
-          <th>Years of Experience</th>
-          <th>Phone Number</th>
-        </thead>
-        <tbody>
-          {filteredAdvocates.map((advocate) => {
-            return (
-              <tr>
-                <td>{advocate.firstName}</td>
-                <td>{advocate.lastName}</td>
-                <td>{advocate.city}</td>
-                <td>{advocate.degree}</td>
-                <td>
-                  {advocate.specialties.map((s) => (
-                    <div>{s}</div>
-                  ))}
-                </td>
-                <td>{advocate.yearsOfExperience}</td>
-                <td>{advocate.phoneNumber}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
     </main>
   );
 }
---
+++ src/app/types/index.d.ts
@@ -0,0 +1,9 @@
+export type Advocate = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  city: string;
+  degree: string;
+  specialties: Array<string>;
+  yearsOfExperience: number;
+};
